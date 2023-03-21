e6f575710bc913bd391a9ad3542d3a30e68f82c6
Rahul Trivedi <trivedirahul1999@gmail.com>
2023-03-21 00:55:16 -0400

    Added cardServices
    Added cardServices

--- Backend/models/product/organization.js
+++ Backend/models/product/organization.js
@@ -1,28 +1,32 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = new Sequelize('mysql::memory:');
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = new Sequelize("mysql::memory:");
 
-class Organizations extends Model { }
+class Organizations extends Model {}
 module.exports = (sequelize, Sequelize) => {
-    const Organization = sequelize.define('Organization', {
-        // Model attributes are defined here
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING(20)
-        },
-        code: {
-            type: DataTypes.STRING(10),
-            unique: true
-        },
-        contact: {
-            type: DataTypes.INTEGER(10)
-        }
-    }, {
-        tableName: 'Organization'
-    });
+  const Organization = sequelize.define(
+    "Organization",
+    {
+      // Model attributes are defined here
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(20),
+      },
+      code: {
+        type: DataTypes.STRING(10),
+        unique: true,
+      },
+      contact: {
+        type: DataTypes.INTEGER(10),
+      },
+    },
+    {
+      tableName: "Organization",
+    }
+  );
 
-    return Organization;
-}
+  return Organization;
+};
