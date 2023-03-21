bd54c34b2a4245eabd948a6c36a253f145fca092
Aakansha Arora <aakansha.a03@gmail.com>
2023-03-21 18:01:28 -0400

    api calls

--- Frontend/src/Components/loginPage.jsx
+++ Frontend/src/Components/loginPage.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../css/loginPage.css";
+import axios from 'axios';
 // import Button from "./Button.js";
 
 function UserLogin(props) {
-  // const email = useFormInput('')
-  // const password = useFormInput('')
+  const user = useRef("")
+  const password = useRef("")
   // const email = "random.email";
   // const password = "12345";
   const [error, setError] = useState(null);
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   const login = () => {
-    axios.post('http://localhost:3005/services/userService/login', { user:email.value, password:password.value}).then((response) => {
-      setUser( response.data);
+    console.log(user)
+    console.log(password)
+    axios.post('http://localhost:3005/auth/login', { email:user.current.value, password:password.current.value}).then((response) => {
+      // setUser( response.data);
+      console.log(response);
       console.log(user);
 
-      localStorage.setItem("user",JSON.stringify(response.data));
-      console.log(JSON.parse(localStorage.getItem("user")).email);
-      console.log(JSON.parse(localStorage.getItem("user")).uid);
+      // localStorage.setItem("user",JSON.stringify(response.data));
+      // console.log(JSON.parse(localStorage.getItem("user")).email);
+      // console.log(JSON.parse(localStorage.getItem("user")).uid);
       // history.push('/');
 
     }).catch(error => {
+      console.log(error);
       if (error.response.status === 401) setError(error.response.data.message);
       else setError("Invalid credentials ");
     });
@@ -29,30 +34,40 @@ function UserLogin(props) {
   return (
     <>
       <main>
-      <form className="login-card">
           <h3 className="heading">FARV Login</h3>
 
           <article className="user-container">
             <label className="username-label">Username</label>
-            <input type="text"></input>
+            <input type="text" ref={user}></input>
           </article>
 
           <article className="password-container">
           <label className="password-label">Password</label>
-            <input type="text"></input>
+            <input type="text" ref={password}></input>
           </article>
           <p className="forgot-password">Forgot password?</p>
             
           <article className="button-container">
-            <button className="submit-button btn btn-success">Submit</button>
+            <button className="submit-button btn btn-success" onClick={login}>Submit</button>
           </article>
           <p className="info-tag">For sign in contact admin of your organization</p>
-        </form>
       </main>
         
     </>
 
   );
+}
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const userChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: userChange
+  }
 }
 
 export default UserLogin;
