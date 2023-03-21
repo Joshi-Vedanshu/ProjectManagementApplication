5593813270d73c3e9e6548373656fefcbdae528f
fermerida <fer.merida94@gmail.com>
2023-03-21 17:04:57 -0400

    Router after login and style

--- Frontend/src/Components/Routing.jsx
+++ Frontend/src/Components/Routing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Project } from "../Projects";
 import UserLogin from "./loginPage";
 import UserSignUp from "./signUp";
 
@@ -11,7 +12,9 @@ export default class Routing extends React.Component {
       <div>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<UserLogin />} />
             <Route path="/login" element={<UserLogin />} />
+            <Route path="/Projects" element={<Project />} />
             <Route path="/signup" element={<UserSignUp/>}/>
           </Routes>
         </BrowserRouter>
