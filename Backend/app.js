bb4a3044d79cfadd7c22b1e2c7657a13759e15c6
Aakansha Arora <aakansha.a03@gmail.com>
2023-03-29 18:30:53 -0400

    ui work

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ var authenticationRouter = require('./routes/authentication');
 
 
 var app = express();
-
+var cors = require('cors')
 
 
 app.use(logger('dev'));
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,6 +41,11 @@ app.use(function (err, req, res, next) {
   res.send({'error':err.message});
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(3005,function(){
   initializeDbConnection();
