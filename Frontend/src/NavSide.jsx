0125b1cded13e2653a57f22bf028aba8e7042207
fermerida <fer.merida94@gmail.com>
2023-04-17 18:09:45 -0400

    Changed sidebar bootstrap to contrast

--- Frontend/src/NavSide.jsx
+++ Frontend/src/NavSide.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import './Style.css'
 import { NavLink } from 'react-router-dom'; // import the NavLink component
-
+import {Nav, NavItem, Navbar, NavDropdown} from 'react-bootstrap';
+import {
+    CDBBadge,
+    CDBSidebar,
+    CDBSidebarContent,
+    CDBSidebarFooter,
+    CDBSidebarHeader,
+    CDBSidebarMenu,
+    CDBSidebarMenuItem,
+   } from "cdbreact";
 
 export default function NavSide() {
 
     return (
+
+    <nav className='navside' >
+ <CDBSidebarContent>
+    <CDBSidebarMenu>
+      <CDBSidebarMenuItem icon="th-large">
+          Dashboard
+       </CDBSidebarMenuItem>
+       <CDBSidebarMenuItem icon="sticky-note">
+          Components
+        </CDBSidebarMenuItem>
+     </CDBSidebarMenu>
+     <CDBSidebarMenu>
         
-        <nav className='navside'>
-            <NavLink to="/" className={({isActive})=>isActive?" active":""}>Home</NavLink>
-            <NavLink to="/about" className={({isActive})=>isActive?" active":""}>Sprints</NavLink>
-            <NavLink to="/events" className={({isActive})=>isActive?" active":""}>Cards</NavLink>
-            <NavLink to="/events" className={({isActive})=>+isActive?" active":""}>Profile</NavLink>
-        </nav>
+    </CDBSidebarMenu>
+ </CDBSidebarContent>
+ 
+    </nav>
+        
         
     );
 
