f6f4d78cdc4f07c85975e4d8d93dcaca1354dc57
fermerida <fer.merida94@gmail.com>
2023-04-17 19:46:44 -0400

    Base of backlog and stories

--- Frontend/src/Components/Routing.jsx
+++ Frontend/src/Components/Routing.jsx
@@ -4,6 +4,8 @@ import { Project } from "../Projects";
 import UserLogin from "./loginPage";
 import UserSignUp from "./signUp";
 import AddProject from "./../AddProject";
+import { Backlog } from "../Backlog";
+import { Sprint } from "../Sprint";
 
 // add your page add Route below between switch and inside route add your component
 
@@ -15,7 +17,9 @@ export default class Routing extends React.Component {
           <Routes>
             <Route path="/" element={<UserLogin />} />
             <Route path="/login" element={<UserLogin />} />
-            <Route path="/Projects" element={<Project />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/backlog" element={<Backlog />} />
+            <Route path="/sprint" element={<Sprint />} />
             <Route path="/signup" element={<UserSignUp/>}/>
             <Route path="/addproject" element={<AddProject/>}/>
           </Routes>
